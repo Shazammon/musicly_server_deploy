ef020502126bb9b2f47c382503f2b18e4427a54a
Shazammon <ammonorgill@gmail.com>
2022-11-01 15:03:09 -0600

    more updates

--- musicly_project/settings.py
+++ musicly_project/settings.py
@@ -187,8 +187,14 @@ SIMPLE_JWT = {
 }
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DISABLE_COLLECTSTATIC = 0
 
 import dj_database_url
 
